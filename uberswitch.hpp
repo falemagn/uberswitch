bffe7a8ba31373e60e724969a4969cd7d1f29aba
Fabio <507164+falemagn@users.noreply.github.com>
2020-08-29 13:59:47 +0200

    Made compatible with c++11, at the cost of a bit of ugliness.

--- uberswitch.hpp
+++ uberswitch.hpp
@@ -8,7 +8,7 @@ namespace uberswitch {
     namespace detail {
     
         template <typename T>
-        using copy_or_ref_t = std::conditional_t<std::is_lvalue_reference_v<T>, T, std::decay_t<T>>;
+        using copy_or_ref_t = typename std::conditional<std::is_lvalue_reference<T>::value, T, typename std::decay<T>::type>::type;
     
     }
 
@@ -23,11 +23,11 @@ namespace uberswitch {
     }
     
     template <typename T, typename... Us>
-    constexpr bool match(const std::tuple<T, Us...> &value, const std::decay_t<T> &t, const std::decay_t<Us> &... us) {
+    constexpr bool match(const std::tuple<T, Us...> &value, const typename std::decay<T>::type &t, const typename std::decay<Us>::type &... us) {
         return value == std::forward_as_tuple(t, us...);
     }
     
-    inline constexpr struct any final {
+    static constexpr struct any final {
         template <typename T>
         friend constexpr bool operator==(const any &, const T &) { return true; }
         
@@ -37,12 +37,14 @@ namespace uberswitch {
 
 }
 
-#define uberswitch(...)                                                              \
-    if (bool uberswitch_matched_ = false; false); else                               \
-    if (std::size_t uberswitch_idx_ = __COUNTER__+1; false); else                    \
-    if (auto uberswitch_value_ = uberswitch::value(__VA_ARGS__); false); else        \
-    for (bool uberswitch_trying_match_ = false; (uberswitch_trying_match_ ^= true);) \
-    switch (uberswitch_idx_)                                                         \
+#define uberswitch(...)                                                                      \
+    for (bool uberswitch_var_init_ = true; uberswitch_var_init_; )                           \
+        for (bool uberswitch_matched_ = false; uberswitch_var_init_;)                        \
+        for (std::size_t uberswitch_idx_ = __COUNTER__+1; uberswitch_var_init_;)             \
+        for (auto uberswitch_value_ = uberswitch::value(__VA_ARGS__); uberswitch_var_init_;) \
+    for (;uberswitch_var_init_; uberswitch_var_init_ = false)                                \
+    for (bool uberswitch_trying_match_ = false; (uberswitch_trying_match_ ^= true);)         \
+    switch (uberswitch_idx_)                                                                 \
 /***/
     
 #define case(...)                                                                                  \
