a336346738a882e112e5537e7b32c25b3205fad3
Fabio <507164+falemagn@users.noreply.github.com>
2020-08-29 09:01:55 +0200

    Use a stateful class.

--- uberswitch.hpp
+++ uberswitch.hpp
@@ -10,46 +10,39 @@ namespace uberswitch {
     namespace detail {
     
         template <typename T>
-        struct is_initializer_list: std::false_type {};
+        using copy_or_ref_t = std::conditional_t<std::is_lvalue_reference_v<T>, T, std::decay_t<T>>;
+    
+    }
+
+    template <typename T, typename... Us>
+    struct state
+    {
+        constexpr state(std::size_t initial_switch, T t, Us ... u)
+            : value_{t, u...}
+            , switch_{initial_switch}
+        {}
         
-        template <typename T>
-        struct is_initializer_list<std::initializer_list<T>>: std::true_type {};
-    
-    }
-
-    template <typename T = void>
-    struct equal_to: std::equal_to<T>{};
-    
-    template <typename T, std::enable_if_t<!detail::is_initializer_list<T>::value>* = nullptr>
-    constexpr bool match(const T &switchVal, const T &caseVal) {        
-        return equal_to<T>()(switchVal, caseVal);
-    }
-
-    template <typename T, typename U>
-    constexpr bool match(const T &switchVal, const U &caseVal) {
-        return equal_to<>()(switchVal, caseVal);
-    }
-    
-    template <typename T, typename U>
-    constexpr bool match(const std::initializer_list<T> &switchVal, const std::initializer_list<U> &caseVal) {
-        return std::equal(std::begin(switchVal), std::end(switchVal), std::begin(caseVal), std::end(caseVal));
-    }
-    
-    template <typename T, template <typename...> typename Tuple, typename... Us, std::size_t... Is>
-    constexpr bool match(const std::initializer_list<T> &switchVal, const Tuple<Us...> &caseVal, std::index_sequence<Is...>) {
-        return sizeof...(Us) == switchVal.size() && ((switchVal.begin()[Is] == std::get<Is>(caseVal)) && ...);        
-    }
+        template <typename... Xs>
+        constexpr bool match(Xs &&... xs) {
+            return matched_ = (value_ == std::forward_as_tuple(std::forward<Xs>(xs)...));
+        }
+        
+        constexpr explicit operator bool() const {
+            return !matched_;    
+            }
+        
+        std::size_t next() {
+            return ++switch_;
+        }
+        
+    private:
+        std::tuple<T, Us...> value_;
+        std::size_t switch_;
+        bool matched_{};
+    };
     
     template <typename T, typename... Us>
-    constexpr bool match(const std::initializer_list<T> &switchVal, const Us &... caseVal) {
-        auto s = switchVal.begin();
-        return sizeof...(Us) == switchVal.size() && ((*(s++) == caseVal) && ...);        
-    }
-    
-    template <typename T, template <typename...> typename Tuple, typename... Us>
-    constexpr bool match(const std::initializer_list<T> &switchVal, const Tuple<Us...> &caseVal) {
-        return match(switchVal, caseVal, std::index_sequence_for<Us...>());        
-    }
+    state(std::size_t initial_switch, T && t, Us &&... us) -> state<detail::copy_or_ref_t<T>, detail::copy_or_ref_t<Us>...>;
     
     inline constexpr struct any final {
         template <typename T>
@@ -57,7 +50,7 @@ namespace uberswitch {
         
         template <typename T>
         friend constexpr bool operator==(const T &, const any &) { return true; }
-    } any;    
+    } any;
     
 }
 
@@ -70,7 +63,7 @@ namespace uberswitch {
 #define uberswitch_match_ uberswitch_label_(match,__LINE__)
 
 #if uberswitch_GCC_VERSION_ >= 70000 && !defined(Q_CREATOR_RUN)
-    // This apparently unexplicable wrapping is due to a GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=85153
+// This apparently unexplicable wrapping is due to a GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=85153
 #   define uberswitch_PragmaWrap_(x) x
 #   define uberswitch_BeginIgnoreWarning_ uberswitch_PragmaWrap_(_Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wswitch-unreachable\""))
 #   define uberswitch_EndIgnoreWarning_ uberswitch_PragmaWrap_(_Pragma("GCC diagnostic pop"))
@@ -79,18 +72,15 @@ namespace uberswitch {
 #   define uberswitch_goto_(x) goto x;
 #endif
 
-#define uberswitch(...)                                           \
-    if (const auto &uberswitch_val_ = __VA_ARGS__; false); else   \
-    if (std::size_t uberswitch_state_ = __COUNTER__; false); else \
-    if (bool uberswitch_matched_ = false; false); else            \
-    while (!uberswitch_matched_)                                  \
-    switch(++uberswitch_state_)                                   \
+#define uberswitch(...)                                                                     \
+    for (uberswitch::state uberswitch_state_(__COUNTER__, __VA_ARGS__); uberswitch_state_;) \
+    switch(uberswitch_state_.next())                                                        \
 /***/
 
-#define case(...)                                                                                   \
-                      uberswitch_goto_(uberswitch_match_)                                           \
-    case __COUNTER__: if (!(uberswitch_matched_ = uberswitch::match(uberswitch_val_, __VA_ARGS__))) \
-                          break;                                                                    \
-    uberswitch_match_                                                                               \
+#define case(...)                                                \
+                      uberswitch_goto_(uberswitch_match_)        \
+    case __COUNTER__: if (!uberswitch_state_.match(__VA_ARGS__)) \
+                          break;                                 \
+    uberswitch_match_                                            \
 /***/
 #endif //!UBERSWITCH_HPP_
